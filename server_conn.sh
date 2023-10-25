f459357a30c77efef5ad4a2e614e0b063d406698
zhudeping <zhudeping_buu@163.com>
2023-10-25 17:37:10 +0800

    update

--- server_conn.sh
+++ server_conn.sh
@@ -3,10 +3,11 @@
 set timeout -1
 set user [lindex $argv 0]
 set ip [lindex $argv 1]
-set password [lindex $argv 2]
-set user_cmd [lindex $argv 3]
+set port [lindex $argv 2]
+set password [lindex $argv 3]
+set user_cmd [lindex $argv 4]
 
-spawn ssh ${user}@${ip}
+spawn ssh ${user}@${ip} -p ${port}
 expect {
     "yes/no" {
         send "yes\r"
