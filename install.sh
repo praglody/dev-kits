f9337327e72b6263fc391a61063878ae4afb3cf3
zhudeping <zhudeping_buu@163.com>
2023-10-25 18:13:02 +0800

    update

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 Cur_dir=$(pwd)
 
-ln -sf $Cur_dir/de /usr/local/bin/de
-ln -sf $Cur_dir/dp /usr/local/bin/dp
-ln -sf $Cur_dir/root /usr/local/bin/root
+#ln -sf $Cur_dir/de /usr/local/bin/de
+#ln -sf $Cur_dir/dp /usr/local/bin/dp
+#ln -sf $Cur_dir/root /usr/local/bin/root
 ln -sf $Cur_dir/jump /usr/local/bin/jump
-ln -sf $Cur_dir/devhome /usr/local/bin/devhome
+#ln -sf $Cur_dir/devhome /usr/local/bin/devhome
 
 exit
 
